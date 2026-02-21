ec1c7523e82c9e322b8dff1f1fb1686d60541492
galileobatallaR <galileo1324@hotmail.com>
2026-02-21 15:19:51 -0600

    Update ej5.c

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -4,7 +4,7 @@ int main(){
     // Resolver ejercicio 5
 
 int a[10];
-    printf("&a[0] = %p\n, &a[0]); 
-        printf("&a[10] = %p\n, &a[10]); 
+    printf("&a[0] = %p\n", &a[0]); 
+        printf("&a[10] = %p\n", &a[10]); 
     return 0;
 }
