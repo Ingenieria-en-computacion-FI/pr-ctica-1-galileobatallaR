f62f11d357bcf48a31a7983465285f62a9c7035c
galileobatallaR <galileo1324@hotmail.com>
2026-02-21 11:47:22 -0600

    Update ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,9 @@
-
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 9
+    int a[5] = {10, 20, 30, 40, 50};
+    printf("Direccion &a[-1]: %p\n", (void*)&a[-1]);
+    printf("Direccion &a[5]: %p\n", (void*)&a[5]);
+        
     return 0;
 }
