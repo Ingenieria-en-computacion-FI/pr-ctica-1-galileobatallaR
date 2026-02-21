68ac2d43b041335a6c39b29775ab8c785073bd44
galileobatallaR <galileo1324@hotmail.com>
2026-02-21 11:53:06 -0600

    Update ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,9 @@
-
 #include <stdio.h>
+int gigante[10000000];
+
 int main(){
     // Resolver ejercicio 10
+    gigante[0] = 1
+    printf("declarar la matriz como global hace que se almacene en segmento de datos, no en el stack y permite tamaños mayores de memoria.\n"); 
     return 0;
 }
