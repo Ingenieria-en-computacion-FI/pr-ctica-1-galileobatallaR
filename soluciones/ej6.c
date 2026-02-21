a6a4b05a8ba9e41e270b99774704a4a6da2db6df
galileobatallaR <galileo1324@hotmail.com>
2026-02-21 11:30:19 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,12 @@
-
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 6
+    int m[2][4]; 
+
+    for (int i = 0; i<2; i++){ 
+        for (int j = 0; j<4; j++){ 
+      printf("la direccion de m[%d][%d] es: %p\n", i,j, &m[i][j]); 
+    }    
+}     
     return 0;
 }
