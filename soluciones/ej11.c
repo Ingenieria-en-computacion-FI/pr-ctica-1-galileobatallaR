4894e9d5ad67897c01e8a51ca3c37982b8e262d6
galileobatallaR <galileo1324@hotmail.com>
2026-02-21 10:08:59 -0600

    Update ej11.c

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,15 @@
-
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 11
-    return 0;
+double a[3]; 
+
+printf("--Ejercicio 11--\n");
+printf("tamaño del tipo: %zu bytes\n", sizeof(double));  
+for (int i=0; i < 3; i++){ 
+    printf("&a[%d] = %p (distancia desde el inicio: %ld bytes)\n", i, (void*)&a[i], (char*)&a[i] - (char*)&a[0]); 
+}
+    printf("Interpretacion: la diferencia entre &a[0] y &a[1] es 8,\n"); 
+    printf("entonces el tipo de dato es de 8 bytes, como un double o un long.\n"); 
+    
+        return 0;
 }
