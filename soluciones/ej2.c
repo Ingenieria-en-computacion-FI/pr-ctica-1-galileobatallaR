f0b866081f1d9c5a5e11bf1b863ebcf205f7903a
galileobatallaR <galileo1324@hotmail.com>
2026-02-21 15:08:39 -0600

    Update ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -2,5 +2,13 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 2
+int a[10];
+    printf("&a[4] = %p\n" , &a[4]);
+    printf("&a[5] = %p\n" , &a[5]);
+if(&a[4]>&a[5]);
+    printf("es mayor la direccion de: &a[4]\n");
+    else
+    printf("es mayor la direccion de: &a[5]\n");
+    
     return 0;
 }
